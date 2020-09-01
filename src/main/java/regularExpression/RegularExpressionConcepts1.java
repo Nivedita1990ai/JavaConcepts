0aac552ddbe07698488e4c6fdfd05cd71bb95a7f
nivedita1990ai <1717ni@gmail.com>
2020-08-31 22:22:25 -0400

    RegEx update Commit

--- src/main/java/regularExpression/RegularExpressionConcepts1.java
+++ src/main/java/regularExpression/RegularExpressionConcepts1.java
@@ -2,4 +2,9 @@ package regularExpression;
 
 public class RegularExpressionConcepts1 {
 
+	public static void main(String[] args) {
+		//Challenge 1
+		String str = "I want a bike.";
+		System.out.println(str.matches("[A-Z][ ][a-z]{4}[ ][a-z][ ][a-z]{4}['.']"));
+	}
 }
